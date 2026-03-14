38a42a0d8bc850d0d46090f0cc425e378d5ead79
dffrs <dffrs@icloud.com>
2026-03-14 19:25:40 +0000

    small example to scrap data out of bambulabs

--- cmd/scrappy.go
+++ cmd/scrappy.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/gocolly/colly/v2"
+)
+
+const url = "https://eu.store.bambulab.com/products/a1?from=navigation&id=599117150694776840"
+
+var re = regexp.MustCompile(`(\d)+\sEUR`)
 
 func main() {
-	fmt.Print("hello world")
+	c := colly.NewCollector(
+		colly.CacheDir("./cache/bbl"),
+	)
+
+	c.OnHTML("span", func(h *colly.HTMLElement) {
+		if re.MatchString(h.Text) {
+			fmt.Printf("Price: %v\n", h.Text)
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting: %s\n", r.URL.String())
+
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Android 12; Mobile; rv:109.0) Gecko/113.0 Firefox/113.0")
+		r.Headers.Set("X-Bbl-Store-Region", "EU")
+		r.Headers.Set("X-Bbl-Time-Zone", "Europe/Lisbon")
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	if err := c.Visit(url); err != nil {
+		panic(err)
+	}
 }
